fa6c9232293075a2eb334376f9e1c15c521c70ed
Rei Lin <temisrei@gmail.com>
2019-08-20 09:18:01 +0800

    將資料送到 Line 群組

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ $(function () {
   var btnLIFF = $('#btnLIFF');
   var answer = $('#answer');
 
+  liff.init(function (data) {
+    console.log(data.context.userId);
+    answer.html(answer.html() + '<br>' + data.context.userId);
+  });
+
   var getIrisData = function () {
     iris = sepalLength.val() + ',' + sepalWidth.val() + ',' + petalLength.val() + ',' + petalWidth.val();
     console.log(iris);
@@ -21,11 +26,22 @@ $(function () {
   btnPredict.click(function () {
     getIrisData();
     $.ajax({
-      url: "https://serene-caverns-64322.herokuapp.com/api?iris=" + iris,
+      url: "[改為自己的 Github Pages 前端網址]/api?iris=" + iris,
       success: function (response) {
         console.log(response);
         answer.html(response.result);
       }
     });
+  });
+
+  btnLIFF.click(function () {
+    console.log(answer.html());
+    liff.sendMessages([
+      {
+        type: 'text',
+        text: answer.html(),
+      }
+    ]);
+    liff.closeWindow();
   });
 });
